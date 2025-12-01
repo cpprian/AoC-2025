78499b68b9979236caf1f0b7c964f9f2f6b42f8b
Cyprian Szczepański <cyprian@Cyprians-MacBook-Pro.local>
2025-12-01 16:54:44 +0100

    init day01

---
+++ day01/src/main.rs
@@ -0,0 +1,10 @@
+use anyhow::{Context, Result};
+
+fn solve_part1(input: &str) -> Result<i32> {
+    Ok(0)
+}
+
+fn main() -> Result<()> {
+    println!("Hello, world!");
+    Ok()
+}
---
+++ day01/target/debug/deps/day01-7f39ef9d24363f6e.d
@@ -0,0 +1,9 @@
+/Users/cyprian/repo/AoC-2025/day01/target/debug/deps/day01-7f39ef9d24363f6e.d: src/main.rs Cargo.toml
+
+/Users/cyprian/repo/AoC-2025/day01/target/debug/deps/libday01-7f39ef9d24363f6e.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
